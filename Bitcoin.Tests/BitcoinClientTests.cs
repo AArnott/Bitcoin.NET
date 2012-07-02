9bb256d1ec525936d7bb37c534c857b5cc993ad8
Andrew Arnott <andrewarnott@gmail.com>
2012-07-01 19:37:49 -0700

    Bitcoin client initial commit

---
+++ Bitcoin.Tests/BitcoinClientTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Bitcoin.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class BitcoinClientTests {
+	}
+}
---
+++ Bitcoin/BitcoinClient.cs
@@ -0,0 +1,10 @@
+﻿namespace Bitcoin {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class BitcoinClient {
+	}
+}
